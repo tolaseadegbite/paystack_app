33e8ac96fe16a3e45cdd56cda87a4c23b55b87ac
tolaseadegbite <tolasekelvin@gmail.com>
2023-08-20 21:35:20 +0100

    Paystack checkout session

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,20 @@
+class CheckoutController < ApplicationController
+    
+    def create
+        product = Product.find(params[:id])
+
+        @paystack = Paystack.new(Rails.application.credentials[:paystack][:PAYSTACK_PUBLIC_KEY],Rails.application.credentials[:paystack][:PAYSTACK_PRIVATE_KEY])
+
+        @payment = PaystackTransactions.new(@paystack)
+        @result = @payment.initializeTransaction(
+            :name => product.name,
+            :reference => "#{product.id}__#{rand.to_s[2..10]}",
+            :amount => product.price * 100,
+            :email => "tolasekevin@gmail.com"
+            )
+        auth_url = @result['data']['authorization_url']
+        respond_to do |format|
+            format.html { redirect_to auth_url, allow_other_host: true }
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :products
   root "products#index"
+
+  post "checkout/create", to: 'checkout#create'
 end
